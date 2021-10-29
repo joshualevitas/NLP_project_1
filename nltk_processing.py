f601538ca6fad8b58a5c2f7416241c17dadf0452
joshualevitas <jnl@u.northwestern.edu>
2021-10-29 13:01:25 -0500

    name extractor starting to work

--- nltk_processing.py
+++ nltk_processing.py
@@ -2,13 +2,32 @@ import nltk
 from nltk.tokenize import word_tokenize
 from nltk.tag import pos_tag
 
+# pattern = 'NP: {<DT>?<JJ>*<NN>}'
+# cp = nltk.RegexpParser(pattern)
+
 def preprocess(sent):
+    ##only works if names are capitalized
     sent = nltk.word_tokenize(sent)
     sent = nltk.pos_tag(sent)
-    return sent
+    names = []
+    movies = []
+
+    for word in range(len(sent)-1):
+        if sent[word][1] == 'NNP':
+            if sent[word+1][1] == 'NNP':
+                names.append(sent[word][0] + " " + sent[word+1][0])
+            else:
+                #figure out how to remove duplicate^
+                movies.append(sent[word][0])
+
+
+
+    return names, movies
+
+
 
 
 
 ## tests
-txt = "Jennifer Lopez's dress is jaw droppingly amazing #GoldenGlobes #redcarpet"
+txt = "Jennifer Lopez's dress is jaw Argo droppingly amazing #GoldenGlobes #redcarpet"
 print(preprocess(txt))
